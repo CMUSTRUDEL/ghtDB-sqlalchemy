3ec14f31e5cf72beb3397e639b56a06d3454ce67
Bogdan Vasilescu <vasilescu@gmail.com>
2020-06-23 22:21:14 -0400

    namsor origin

--- ghtdb_sqlalchemy/__init__.py
+++ ghtdb_sqlalchemy/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ['projects', 'name']
+__all__ = ['projects', 'name', 'namsor_origin']
 # from .initDB import initDB
 from .base import Base
 from .projects import Project
 from .name import Name
+from .namsor_origin import NamSorOrigin
