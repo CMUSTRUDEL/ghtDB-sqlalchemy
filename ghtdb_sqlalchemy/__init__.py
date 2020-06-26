78208613ebae9f81c8d6ce5bc90c0d35bee2edc7
Bogdan Vasilescu <vasilescu@gmail.com>
2020-06-25 22:28:07 -0400

    Added gender table

--- ghtdb_sqlalchemy/__init__.py
+++ ghtdb_sqlalchemy/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['projects', 'name', 'namsor_origin']
+__all__ = ['projects', 'name', 'namsor_origin', 'namsor_gender']
 # from .initDB import initDB
 from .base import Base
 from .projects import Project
 from .name import Name
 from .namsor_origin import NamSorOrigin
+from .namsor_gender import NamSorGender
---
+++ ghtdb_sqlalchemy/namsor_gender.py
@@ -0,0 +1,34 @@
+import sqlalchemy as sa
+from sqlalchemy import Column, Table
+from sqlalchemy import Integer, String, Boolean, BigInteger, DateTime, Text, Numeric
+from .base import Base
+
+
+class NamSorGender(Base):
+    __tablename__ = 'bv_namsor_gender_geo_manual_first_last_2_0_10'
+    
+    id = Column(Integer, primary_key=True)
+    name_id = Column(Integer)
+    likely_gender = Column(String)
+    gender_scale = Column(Numeric)
+    score = Column(Numeric)
+    probability_calibrated = Column(Numeric)
+    
+    def __init__(self, 
+                name_id,
+                likely_gender,
+                gender_scale,
+                score,
+                probability_calibrated):
+        self.name_id = name_id
+        self.likely_gender = likely_gender
+        self.gender_scale = gender_scale
+        self.score = score
+        self.probability_calibrated = probability_calibrated
+
+
+    def __repr__(self):
+        return 'Name gender: %d - %s' % \
+                    (self.name_id, self.likely_gender) 
+    
+
