5ea0fd8a3a1b9637654ee8176de8a3f4bd0d917f
Bogdan Vasilescu <vasilescu@gmail.com>
2020-06-30 22:08:28 -0400

    new gender table

--- ghtdb_sqlalchemy/__init__.py
+++ ghtdb_sqlalchemy/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ['projects', 'name', 'namsor_origin', 'namsor_gender', 'bv_committers']
+__all__ = ['projects', 'name', 'namsor_origin', 'namsor_gender', 'namsor_gender_full', 'bv_committers']
 # from .initDB import initDB
 from .base import Base
 from .projects import Project
 from .name import Name
 from .namsor_origin import NamSorOrigin
 from .namsor_gender import NamSorGender
+from .namsor_gender_full import NamSorGenderFull
 from .bv_committers import BVCommitter
---
+++ ghtdb_sqlalchemy/namsor_gender_full.py
@@ -0,0 +1,37 @@
+import sqlalchemy as sa
+from sqlalchemy import Column, Table, ForeignKey
+from sqlalchemy import Integer, String, Boolean, BigInteger, DateTime, Text, Numeric
+from .base import Base
+from sqlalchemy.orm import relationship
+
+
+class NamSorGenderFull(Base):
+    __tablename__ = 'bv_namsor_gender_full_2_0_10'
+    
+    id = Column(Integer, primary_key=True)
+    name_id = Column(Integer, ForeignKey("bv_names.id"))
+    likely_gender = Column(String)
+    gender_scale = Column(Numeric)
+    score = Column(Numeric)
+    probability_calibrated = Column(Numeric)
+
+    gh_name = relationship('Name', backref='bv_namsor_gender_full_2_0_10')
+    
+    def __init__(self, 
+                name_id,
+                likely_gender,
+                gender_scale,
+                score,
+                probability_calibrated):
+        self.name_id = name_id
+        self.likely_gender = likely_gender
+        self.gender_scale = gender_scale
+        self.score = score
+        self.probability_calibrated = probability_calibrated
+
+
+    def __repr__(self):
+        return 'Name gender: %d - %s' % \
+                    (self.name_id, self.likely_gender) 
+    
+
