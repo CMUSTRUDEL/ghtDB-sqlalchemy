8576421dee18212884288b6d5477fcc294ec43d9
Bogdan Vasilescu <vasilescu@gmail.com>
2020-08-27 11:44:43 -0400

    Stars

---
+++ ghtdb_sqlalchemy/ght_star.py
@@ -0,0 +1,25 @@
+import sqlalchemy as sa
+from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy import Integer, String, Text, Boolean, BigInteger, DateTime, Numeric
+from sqlalchemy.dialects.mysql import TIMESTAMP
+from sqlalchemy.orm import relationship, backref
+#from sqlalchemy.ext.associationproxy import association_proxy
+import datetime
+from .base import Base
+from dateutil import parser
+    
+
+class GHTStar(Base):
+    __tablename__ = 'watchers'
+    
+    repo_id = Column(Integer)
+    user_id = Column(Integer)
+    created_at = Column(DateTime(timezone=True))
+
+
+    def __repr__(self):
+        return 'GH star: %s - %s - %s' % \
+                    (self.repo_id, self.user_id, self.created_at) 
+
+
+    
